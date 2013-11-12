d176c8ee5f0a8349366e3d4b5c434309155bb582
Theonik <theonik2006@hotmail.com>
2013-11-12 02:34:32 +0000

    Implemented downwards and rightwards movement of space.

--- src/su/kininarima/sliderpuzzle/main/BoardState.java
+++ src/su/kininarima/sliderpuzzle/main/BoardState.java
@@ -1,10 +1,20 @@
 package su.kininarima.sliderpuzzle.main;
 
 public class BoardState {
-	private byte spacePos;
+	private static final byte sizeX = 3;
+	private static final byte sizeY = 3;
+	private byte spacePosY;
+	private byte spacePosX;
 	private byte[][] board;
 	public BoardState() {
-		// TODO Auto-generated constructor stub
+		board = new byte[sizeX][sizeY];
+	}
+	
+	public BoardState(BoardState b, Direction d){
+		this.spacePosY = b.spacePosY;
+		this.spacePosX = b.spacePosX;
+		this.board = b.board;
+		moveSpace(d);
 	}
 	
 	public boolean isEqual(BoardState other){
@@ -16,6 +26,40 @@ public class BoardState {
 			}
 		}
 		return true;
+	}
+	
+	private boolean moveSpace(Direction d) {
+		switch (d){
+		case UP:
+			if (spacePosY>0){
+				board[spacePosX][spacePosY] = board[spacePosX][spacePosY-1];
+				spacePosY--;
+				board[spacePosX][spacePosY] = 0;
+				return true;
+			}
+		case DOWN:
+			if (spacePosY<(sizeY-1)) {
+				board[spacePosX][spacePosY] = board[spacePosX][spacePosY+1];
+				spacePosY++;
+				board[spacePosX][spacePosY] = 0;
+				return true;
+			}
+		case LEFT:
+			if (spacePosX>0){
+				board[spacePosX][spacePosY] = board[spacePosX-1][spacePosY];
+				spacePosX--;
+				board[spacePosX][spacePosY] = 0;
+				return true;
+			}
+		case RIGHT:
+			if (spacePosX<(sizeX-1)) {
+				board[spacePosX][spacePosY] = board[spacePosX+1][spacePosY];
+				spacePosX++;
+				board[spacePosX][spacePosY] = 0;
+				return true;
+			}
+		}
+		return false;		
 	}
 
 }
