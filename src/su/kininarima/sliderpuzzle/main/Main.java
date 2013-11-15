f771bf7f2ccd5cbf8cc3f403d54f080e11d77b78
Theonik <theonik2006@hotmail.com>
2013-11-15 19:56:36 +0000

    Completed Main method skeleton.

--- src/su/kininarima/sliderpuzzle/main/Main.java
+++ src/su/kininarima/sliderpuzzle/main/Main.java
@@ -1,13 +1,39 @@
 package su.kininarima.sliderpuzzle.main;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class Main {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		FileIO loader = new FileIO();
+		try {
+		BoardState startState = loader.loadPuzzle(new File(args[0]));
+		BoardState endState = loader.loadPuzzle(new File(args[1]));
+		}
+		catch (FileNotFoundException e) {
+			System.out.println("Error File Not Found");
+			System.exit(1);
+		}
+		if (args[2].equals("bfs")) {
+			
+		}
+		else if (args[2].equals("dfs")) {
+			
+		}
+		else if (args[2].equals("astar1")) {
+			
+		}
+		else if (args[2].equals("astar2")) {
+			
+		}
+		else {
+			System.out.println("Error: Invalid mode arguement, acceptable arguements are 'bfs', 'dfs', 'astar1', 'astar2'");
+			System.exit(1);
+		}
 	}
 
 }
