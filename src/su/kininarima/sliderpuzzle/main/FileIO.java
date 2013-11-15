a572a8fdbc63a09686971a782cd8f2468caf7b25
Theonik <theonik2006@hotmail.com>
2013-11-15 23:40:12 +0000

    Fixed bugs in the FileIO class.

--- src/su/kininarima/sliderpuzzle/main/FileIO.java
+++ src/su/kininarima/sliderpuzzle/main/FileIO.java
@@ -15,21 +15,21 @@ public class FileIO {
 
 	public BoardState loadPuzzle(File file) throws FileNotFoundException {
 		byte[][] board = new byte[3][3];
-		byte spaceX = (Byte) null;
-		byte spaceY = (Byte) null;
+		byte spaceX = -1;
+		byte spaceY = -1;
 		Scanner infile =new Scanner(new InputStreamReader
 				(new FileInputStream(file)));
 		for (int y = 0; y<3; y++){
 			String s = infile.nextLine();
-			for (int i = 0;y<s.length(); i++){
-				int x = 0;
+			int x = 0;
+			for (int i = 0;i<s.length(); i++){
 				if (s.charAt(i) == '0'){
 					spaceX = (byte) x;
 					spaceY = (byte) y;
 					board[x][y] = 0;
 					x++;
 				}
-				if (s.charAt(i) == ',') {}
+				else if (s.charAt(i) == ',') {}
 				else {
 					board[x][y] = (byte)(s.charAt(i)-48);
 					x++;
