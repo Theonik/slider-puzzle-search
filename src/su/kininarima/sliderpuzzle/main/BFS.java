cbd9bf0c4fdc2981dd58cbee2bba32385cbd871c
Theonik <theonik2006@hotmail.com>
2013-11-16 19:54:03 +0000

    Added runtime diagnostics to BFS

--- src/su/kininarima/sliderpuzzle/main/BFS.java
+++ src/su/kininarima/sliderpuzzle/main/BFS.java
@@ -19,23 +19,27 @@ public class BFS {
 		BoardState cState = sState;
 		visitedList = new ArrayList<BoardState>();
 		frontierList = new LinkedBlockingQueue<BoardState>();
+		long startTime, endTime, totalTime;
+		startTime = System.nanoTime();
 		while (cState != null && !cState.equals(eState)){
 			if (!visitedList.contains(cState)) {
 				frontierList.add(new BoardState(cState, Direction.UP));
 				frontierList.add(new BoardState(cState, Direction.DOWN));
 				frontierList.add(new BoardState(cState, Direction.LEFT));
 				frontierList.add(new BoardState(cState, Direction.RIGHT));
+				visitedList.add(cState);
 				nodesExpanded++;
 			}
-			visitedList.add(cState);
 			cState = frontierList.poll();
 		}
+		endTime = System.nanoTime();
+		totalTime = endTime - startTime;
 		if (cState == null) {
-			System.out.println("Could not find solution after expanding " + nodesExpanded + " nodes.");
+			System.out.println("Could not find solution after expanding " + nodesExpanded + " nodes. Searched for " + totalTime*1E-6 + "ms.");
 		}
 		else {
 			Display out = new Display();
-			System.out.println("Solution found after expanding "+ nodesExpanded + " nodes.");
+			System.out.println("Solution found after expanding "+ nodesExpanded + " nodes. Searched for " + totalTime*1E-6 + "ms.");
 			System.out.println(out.solutionToString(cState));
 		}
 	}
